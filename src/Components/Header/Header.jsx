64ffedd1d27d0e87abbcec795ea29a6ee79a4c29
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-11-01 11:02:32 +0600

    active class and status

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
@@ -6,11 +6,13 @@ const Header = () => {
         <div>
             <h2>Navbar</h2>
             <nav>
-                <Link to='/'>Home</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/contact'>Contact Us</Link>
-                <Link to='/users'>Users info</Link>
-                <Link to='/posts'>Posts</Link>
+                <NavLink to='/'>Home</NavLink>
+
+                <NavLink to='/about'>About</NavLink>
+
+                <NavLink to='/contact'>Contact Us</NavLink>
+                <NavLink to='/users'>Users info</NavLink>
+                <NavLink to='/posts'>Posts</NavLink>
             </nav>
         </div>
     );
